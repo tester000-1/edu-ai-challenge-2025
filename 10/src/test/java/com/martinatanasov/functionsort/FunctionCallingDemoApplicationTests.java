e58bff0d12b3dafecf33c71826e3b2e00a15555d
Martin.Atanasov <martin.atanasov@ventionteams.com>
2025-06-18 17:10:07 +0300

    Add Task 10

---
+++ 10/src/test/java/com/martinatanasov/functionsort/FunctionCallingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinatanasov.functionsort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionCallingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
